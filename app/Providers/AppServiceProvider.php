a37137cab034fd5976d5055b007e38843f643f57
Ferry Ardiansyah <ferard.diansyah@gmail.com>
2026-04-04 20:44:48 +0700

    hapus register

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -38,6 +38,7 @@ class AppServiceProvider extends ServiceProvider
             $requestUri = $_SERVER['REQUEST_URI'] ?? '';
             
             $path = '';
+            // Jika diakses lewat /efktp, pastikan base URL juga mengandung /efktp
             if (strpos($requestUri, '/efktp') === 0) {
                 $path = '/efktp';
             }
@@ -45,21 +46,23 @@ class AppServiceProvider extends ServiceProvider
             $dynamicUrl = "$protocol://$host$path";
             config(['app.url' => $dynamicUrl]);
 
-            // 3. LOGIKA ASSET:
-            // Kita cek SCRIPT_NAME. Jika SCRIPT_NAME mengandung '/public/index.php',
-            // artinya index.php yang dijalankan memang berada di subfolder public.
-            $scriptName = $_SERVER['SCRIPT_NAME'] ?? '';
-            
-            // Cek apakah URL yang sedang diakses saat ini mengandung '/public/'
-            // Jika kita mengakses lewat index.php di root, SCRIPT_NAME biasanya adalah '/index.php' atau '/efktp/index.php'
-            // Jika kita mengakses lewat folder public, SCRIPT_NAME biasanya adalah '/public/index.php' atau '/efktp/public/index.php'
-            
-            if (strpos($scriptName, '/public/index.php') !== false) {
-                // Jika sudah ada 'public' di script yang dijalankan, maka aset dipanggil tanpa /public lagi
+            // 3. LOGIKA ASSET SUPER AKURAT:
+            // Cek lokasi file fisik index.php yang sedang dieksekusi
+            $scriptFileName = realpath($_SERVER['SCRIPT_FILENAME'] ?? '');
+            $rootIndexFile = realpath(base_path('index.php'));
+            $publicIndexFile = realpath(public_path('index.php'));
+
+            // Jika yang jalan adalah index.php di folder PUBLIC (seperti di Docker Sail)
+            if ($scriptFileName === $publicIndexFile) {
                 config(['app.asset_url' => $dynamicUrl]);
-            } else {
-                // Jika index.php yang dijalankan ada di root, maka aset WAJIB pakai /public
+            } 
+            // Jika yang jalan adalah index.php di folder ROOT (seperti di Apache Subfolder)
+            elseif ($scriptFileName === $rootIndexFile) {
                 config(['app.asset_url' => "$dynamicUrl/public"]);
+            }
+            // Fallback default
+            else {
+                config(['app.asset_url' => $dynamicUrl]);
             }
         }
     }
